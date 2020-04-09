9ead6235254b9d129fe5899485433c863a2fdaff
wengzhangtao <zhangtao.weng@nexttao.com>
2020-04-09 22:21:15 +0800

    盘点bugfix

--- base_assets/models/AssetInventory.py
+++ base_assets/models/AssetInventory.py
@@ -4,7 +4,6 @@ from odoo.exceptions import UserError
 from odoo.osv import expression
 from odoo.addons import decimal_precision as dp
 
-
 status = [("draft", "Draft"), ("confirm", "Confirm"), ("done", "Done"), ("cancel", "Cancel")]
 
 
@@ -14,33 +13,33 @@ class AssetInventory(models.Model):
 
     _description = 'Assets Inventory Order'
 
-
     name = fields.Char("Name", copy=False, readonly=True)
     apply_user_id = fields.Many2one("res.users", "Apply Users", default=lambda self: self._uid,
-        readonly=True, states={'draft': [('readonly', False)]})
+                                    readonly=True, states={'draft': [('readonly', False)]})
     date = fields.Datetime("Date", default=fields.Datetime.now,
-        readonly=True, states={'draft': [('readonly', False)]})
+                           readonly=True, states={'draft': [('readonly', False)]})
     date_done = fields.Datetime("Date Done", default=fields.Datetime.now,
-        readonly=True)
+                                readonly=True)
     owner_employee_id = fields.Many2one("hr.employee", "Own Employee",
-        readonly=True, states={'draft': [('readonly', False)]})
+                                        readonly=True, states={'draft': [('readonly', False)]})
     owner_department_id = fields.Many2one("hr.department", "Own Department",
-        readonly=True, states={'draft': [('readonly', False)]})
+                                          readonly=True, states={'draft': [('readonly', False)]})
     employee_id = fields.Many2one("hr.employee", "Employee",
-        readonly=True, states={'draft': [('readonly', False)]})
+                                  readonly=True, states={'draft': [('readonly', False)]})
     department_id = fields.Many2one("hr.department", "Department",
-        readonly=True, states={'draft': [('readonly', False)]})
+                                    readonly=True, states={'draft': [('readonly', False)]})
     location_id = fields.Many2one("stock.location", "Location",
-        readonly=True, states={'draft': [('readonly', False)]})
+                                  readonly=True, states={'draft': [('readonly', False)]})
     state = fields.Selection(status, string="Status", readonly=True, default="draft")
     company_id = fields.Many2one("res.company", "Company", required=True,
-        default=lambda self: self.env['res.company']._company_default_get('asset.picking.order'),
-        readonly=True, states={'draft': [('readonly', False)]})
+                                 default=lambda self: self.env['res.company']._company_default_get(
+                                     'asset.picking.order'),
+                                 readonly=True, states={'draft': [('readonly', False)]})
     inventory_line_ids = fields.One2many("asset.inventory.line", "inventory_id", String="Inventory Lines",
-        readonly=True, states={'draft': [('readonly', False)], 'confirm': [('readonly', False)]})
+                                         readonly=True,
+                                         states={'draft': [('readonly', False)], 'confirm': [('readonly', False)]})
     move_line_ids = fields.One2many("asset.move.line", "inventory_id", String="Move Lines",
-        readonly=True)
-
+                                    readonly=True)
 
     @api.model
     def create(self, values):
@@ -77,7 +76,7 @@ class AssetInventory(models.Model):
         inventory_line_ids = []
         res_asset_ids = res_asset_obj.search(domain)
         for res_asset_id in res_asset_ids:
-            inventory_line_ids.append((0, 0,{
+            inventory_line_ids.append((0, 0, {
                 "asset_id": res_asset_id.id,
                 "code": res_asset_id.code,
                 "name": res_asset_id.name,
@@ -94,7 +93,7 @@ class AssetInventory(models.Model):
 
     @api.multi
     def action_done(self):
-        inventory_line_ids = self.inventory_line_ids.filtered(lambda ili:ili.inventory_result == "draft")
+        inventory_line_ids = self.inventory_line_ids.filtered(lambda ili: ili.inventory_result == "draft")
         if inventory_line_ids:
             raise UserError(_("Please comfirm the inventory line by every lines"))
 
@@ -108,31 +107,31 @@ class AssetInventory(models.Model):
                     "code": inventory_line_id.code,
                     "sn": inventory_line_id.sn,
                     "product_id": inventory_line_id.product_id.id,
-                    "category_id": inventory_line_id.asset_category_id.id,
-                    "value": self.value,
-                    "value_residual": self.value,
-                    "company_id": self.order_id.company_id.id,
-                    "currency_id": self.order_id.currency_id.id,
+                    "category_id": inventory_line_id.category_id.id,
+                    "value": inventory_line_id.value,
+                    "value_residual": inventory_line_id.value,
+                    "company_id": inventory_line_id.inventory_id.company_id.id,
+                    "currency_id": inventory_line_id.inventory_id.company_id.currency_id.id,
                     "date": date_done,
                     "state": "draft",
                 }
                 asset = self.env["res.asset"].action_create(vals)
-                move_line_ids.append({
-                    "name": "Inventory %s"%(inventory_line_id.asset_id.display_name),
+                move_line_ids.append((0, 0, {
+                    "name": "Inventory %s" % (inventory_line_id.asset_id.display_name),
                     "inventory_id": self.id,
                     "asset_id": asset.id,
                     "product_id": asset.product_id.id,
                     "date": self.date_done,
-                    "dest_owner_employee_id": self.owner_employee_id,
-                    "dest_owner_department_id": self.owner_department_id,
-                    "dest_employee_id": self.employee_id,
-                    "dest_department_id": self.department_id,
-                    "dest_location_id": self.location_id,
+                    "dest_owner_employee_id": self.owner_employee_id.id,
+                    "dest_owner_department_id": self.owner_department_id.id,
+                    "dest_employee_id": self.employee_id.id,
+                    "dest_department_id": self.department_id.id,
+                    "dest_location_id": self.location_id.id,
                     "state": "done",
-                })
+                }))
             # 盘亏 资产关闭 字段置空
             elif inventory_line_id.inventory_result == "loss":
-                move_line_ids.append({
+                move_line_ids.append((0, 0, {
                     "name": "Inventory %s" % (inventory_line_id.asset_id.display_name),
                     "inventory_id": self.id,
                     "asset_id": inventory_line_id.asset_id.id,
@@ -144,7 +143,7 @@ class AssetInventory(models.Model):
                     "dest_department_id": False,
                     "dest_location_id": False,
                     "state": "done",
-                })
+                }))
             # 确认 资产无发生变化
             elif inventory_line_id.inventory_result == "comfirm":
                 pass
@@ -154,6 +153,7 @@ class AssetInventory(models.Model):
         if move_line_ids:
             val["move_line_ids"] = move_line_ids
         self.write(val)
+        self.mapped('move_line_ids').action_done()
         return True
 
     @api.multi
@@ -162,31 +162,33 @@ class AssetInventory(models.Model):
         self.inventory_line_ids.write({"state": "cancel"})
         self.move_line_ids.action_cancel()
 
+
 class AssetInventoryLine(models.Model):
     _name = "asset.inventory.line"
 
     inventory_id = fields.Many2one("asset.inventory.order", "Inventory")
     asset_id = fields.Many2one("res.asset", "Asset", readonly=True)
     category_id = fields.Many2one("asset.category", "Categories", required=True, copy=False)
-    product_id = fields.Many2one("product.product", "Product")
-    code = fields.Char("Code", required=True, copy=False,)
-    name = fields.Char("Name", required=True, copy=False,)
+    product_id = fields.Many2one("product.product", "Product", required=True)
+    code = fields.Char("Code", required=True, copy=False, )
+    name = fields.Char("Name", required=True, copy=False, )
     sn = fields.Char("SN", copy=False)
-    inventory_result = fields.Selection([("draft", "Draft"), ("profit", "Profit"), ("loss", "Loss"), ("comfirm", "Comfirm")],
+    inventory_result = fields.Selection(
+        [("draft", "Draft"), ("profit", "Profit"), ("loss", "Loss"), ("comfirm", "Comfirm")],
         string="Inventory Result", default="profit", required=True)
-    value = fields.Float("In Value", digits=dp.get_precision("Product Price"), copy=False,)
-    value_residual = fields.Float("Residual Value", digits=dp.get_precision("Product Price"), copy=False,)
+    value = fields.Float("In Value", digits=dp.get_precision("Product Price"), copy=False, )
+    value_residual = fields.Float("Residual Value", digits=dp.get_precision("Product Price"), copy=False, )
     state = fields.Selection(status, string="Status", readonly=True, releted="asset_id.asset_id",
-        store=True)
+                             store=True)
     company_id = fields.Many2one("res.company", "Company", releted="asset_id.company_id",
-        readonly=True, states={'draft': [('readonly', False)]})
+                                 readonly=True, states={'draft': [('readonly', False)]})
 
     @api.constrains('asset_id', 'inventory_result')
     def check_line(self):
         for line in self.sudo():
             if line.asset_id:
-                if self.inventory_result == "profit":
+                if line.inventory_result == "profit":
                     raise UserError(_("The Asset already exists and can not profit"))
             else:
-                if self.inventory_result == "comfirm":
+                if line.inventory_result == "comfirm":
                     raise UserError(_("The Asset don't exist and cannot be confirmed"))
